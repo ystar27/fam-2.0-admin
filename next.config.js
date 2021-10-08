bfeea88a9c4b95f4c942fc0e030f63a2510dcde2
sebastian <ystar27@yahoo.com>
2021-10-08 15:48:38 +0100

    removed typo in next config file

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   eslint: { ignoreDuringBuilds: true },
   typescript: {
     ignoreBuildErrors:true
-  }
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
     return config
