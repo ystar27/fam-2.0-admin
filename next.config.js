d7c0523122a659c769a942d1119611255a0f5925
sebastian <ystar27@yahoo.com>
2021-10-26 11:00:56 +0100

    removed type checks

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
-    return config
+    return config;
   },
-}
+};
