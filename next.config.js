eba744c996f87b159e104537b4d00d18fdafcf2c
Muizudeen Kusimo <muizz@inspaya.com>
2022-06-22 11:46:35 +0100

    Add environment variables to nextjs config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
@@ -5,5 +7,9 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
     return config;
+  },
+  env: {
+    DEV_BASE_URL: process.env.DEV_BASE_URL,
+    PRD_BASE_URL: process.env.PRD_BASE_URL,
   },
 };
