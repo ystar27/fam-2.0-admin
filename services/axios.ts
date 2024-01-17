30d2911097ac54f6663f04db3174a7c71b1a2534
unknown <imanchesebastian@gmail.com>
2024-01-17 19:54:29 +0100

    changed api url route

--- services/axios.ts
+++ services/axios.ts
@@ -6,34 +6,37 @@ import axios from "axios";
 // 		"Content-Type": "application/json",
 // 	},
 // });
+//   baseURL: "https://testapi.femaleandmore.org/api/v2",
 
 const instance = axios.create({
-  baseURL: "https://testapi.femaleandmore.org/api/v2",
+  // baseURL: "http://localhost:4000/api/v2",
+  baseURL: "http://api.femaleandmore.org/api/v2",
+
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const setAuthToken = (token: string) => {
-	if (typeof window !== "undefined") {
-		instance.defaults.headers.common["Authorization"] = token;
-		window.localStorage.setItem("authorization", token);
-	}
+  if (typeof window !== "undefined") {
+    instance.defaults.headers.common["Authorization"] = token;
+    window.localStorage.setItem("authorization", token);
+  }
 };
 
 export const DeleteAuthToken = () => {
-	if (typeof window !== "undefined") {
-		window.localStorage.removeItem("authorization");
-		axios.defaults.headers["Authorization"] = "";
-	}
+  if (typeof window !== "undefined") {
+    window.localStorage.removeItem("authorization");
+    axios.defaults.headers["Authorization"] = "";
+  }
 };
 
 instance.interceptors.request.use(function (config) {
-	if (typeof window !== "undefined") {
-		const token = window.localStorage.getItem("authorization");
-		config.headers.Authorization = token || null;
-		return config;
-	}
+  if (typeof window !== "undefined") {
+    const token = window.localStorage.getItem("authorization");
+    config.headers.Authorization = token || null;
+    return config;
+  }
 });
 
 export default instance;
